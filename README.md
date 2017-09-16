ba4e638b3b4884e36c37328c484274af7a18ee84
Tom Wilson <tom@jackhq.com>
2017-09-16 09:21:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Next Layout
+
+Simple layout component for next.js that include roboto and tachyons
+
+## Install
+
+``` sh
+yarn add @twilson63/next-layout
+```
+
+### Usage
+
+``` js
+import Layout from '@twilson63/next-layout'
+
+...
+
+
+<Layout title="Hello">
+  ...
+</Layout>
+
+```
+
+## LICENSE
+
+MIT
---
+++ index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default ({ title, children }) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="stylesheet" href="https://unpkg.com/tachyons" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"
+        rel="stylesheet"
+      />
+      <style>
+        {`
+          body {
+            font-family: roboto;
+          }
+        `}
+      </style>
+    </Head>
+    {children}
+  </div>
+)
